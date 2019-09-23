7fe78c8bd616785a9429f28be0686db445a3d3af
Chris Wilson <40901561+EnergizedBlast@users.noreply.github.com>
2019-09-23 03:45:34 -0400

    Add Gradle to project, add basic calculator UI

---
+++ src/test/java/BinaryCalculatorTest.java
@@ -0,0 +1,23 @@
+import cs495.BinaryCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class BinaryCalculatorTest {
+
+    @Test
+    public void testBinaryAdd(){
+        BinaryCalculator calculator = new BinaryCalculator();
+        String result = calculator.add("1010", "100");
+        assertEquals(result,"1110");
+    }
+
+    @Test
+    public void testBinaryAddTemp(){
+        BinaryCalculator calculator = new BinaryCalculator();
+        String result = calculator.add("111", "111");
+        assertEquals(result,"1110");
+    }
+}
