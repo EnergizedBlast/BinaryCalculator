08d5dcc5d6fb57b5f2ecd6d38e2e7a17fc128adf
Brysonleeward <brysonleeward@gmail.com>
2019-09-24 00:32:44 -0400

    Added the squared function and squared test. Still need to implement the button.

--- src/main/model/BinaryCalculator.java
+++ src/main/model/BinaryCalculator.java
@@ -2,6 +2,8 @@ package main.model;
 
 public class BinaryCalculator {
 
+    private String s1;
+
     public String add(String s1, String s2){
         int decimal1 = Integer.parseInt(s1,2);
         int decimal2 = Integer.parseInt(s2,2);
@@ -26,6 +28,11 @@ public class BinaryCalculator {
         int decimal1 = Integer.parseInt(s1,2);
         int decimal2 = Integer.parseInt(s2,2);
         int answer = decimal1/decimal2;
+        return Integer.toBinaryString(answer);
+    }
+    public String square(String s1){
+        int decimal1 = Integer.parseInt(s1,2);
+        int answer = decimal1*decimal1;
         return Integer.toBinaryString(answer);
     }
 }
--- test/BinaryCalculatorTest.java
+++ test/BinaryCalculatorTest.java
@@ -1,32 +1,41 @@
 import main.model.BinaryCalculator;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BinaryCalculatorTest {
+class BinaryCalculatorTest {
 
     @Test
-    public void testBinaryAdd(){
+    void testBinaryAdd(){
         BinaryCalculator calculator = new BinaryCalculator();
         String result = calculator.add("1010", "100");
         assertEquals(result,"1110");
     }
 
+    private void assertEquals(String result, String s) {
+    }
+
     @Test
-    public void testBinarySubtract(){
+    void testBinarySubtract(){
         BinaryCalculator calculator = new BinaryCalculator();
         String result = calculator.subtract("10000", "1000");
         assertEquals(result,"1000");
     }
     @Test
-    public void testBinaryMultiply(){
+    void testBinaryMultiply(){
         BinaryCalculator calculator = new BinaryCalculator();
         String result = calculator.multiply("100", "100");
         assertEquals(result,"10000");
     }
     @Test
-    public void testBinaryDivide(){
+    void testBinaryDivide(){
         BinaryCalculator calculator = new BinaryCalculator();
         String result = calculator.divide("11110", "00011");
         assertEquals(result,"1010");
+    }
+
+    @Test
+    void testBinarySquare() {
+        BinaryCalculator calculator = new BinaryCalculator();
+        String result = calculator.square( "111");
+        assertEquals(result, "0110001");
     }
 }
